fbdc961945586ab9808f3926514a69c4a80b228b
Daniel Mircea <daniel@viseztrance.com>
2021-12-23 16:03:04 +0200

    End of Part 1

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 require_relative "log_parser/version"
+require_relative "log_parser/file_reader"
+require_relative "log_parser/entry"
+require_relative "log_parser/tokenizer"
 
 module LogParser
   class Error < StandardError; end
